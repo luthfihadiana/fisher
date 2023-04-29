a0e7ed62676da478d23b7c09cbb086b73152a021
Luthfi Hadiana <luthfihadiana@gmail.com>
2023-04-30 04:34:21 +0700

    feat(base project): update base project
    Add header, font family, scss, base styling

---
+++ src/components/navbar/index.js
@@ -0,0 +1,9 @@
+import styles from './index.module.scss';
+
+export default function Navbar() {
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>Fisher!</h1>
+    </header>
+  );
+}
